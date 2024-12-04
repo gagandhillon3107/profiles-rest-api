139d509e7577c2b0ce5257ecb7facf3f640870aa
Gagan Dhillon <gagandhillon3107@gmail.com>
2024-12-03 19:15:50 -0700

    add profile api

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,9 +6,10 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register("hello-viewset", views.HelloViewSet, basename="hello-viewset")
-router.register("profile", views.UserprofileViewset)
+router.register("profile", views.UserProfileViewset)
 
 urlpatterns = [
     path("hello-view/", views.HelloAPIView.as_view()),
+    path("login/", views.UserLoginAPIView.as_view()),
     path("", include(router.urls)),
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from rest_framework import filters
 
 from profiles_api import models
@@ -67,10 +69,14 @@ class HelloViewSet(viewsets.ViewSet):
         return Response({"http_method": "delete"})
 
 
-class UserprofileViewset(viewsets.ModelViewSet):
+class UserProfileViewset(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnUser,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ["email", "name"]
+
+
+class UserLoginAPIView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
